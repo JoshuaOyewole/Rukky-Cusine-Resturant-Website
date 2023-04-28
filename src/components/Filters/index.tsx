de0defcf3d46899a5b114d07f8576fa29f44353c
joshuaOyewole <joshpp2013@gmail.com>
2023-04-28 11:41:50 +0100

    modified the functionality for filtering items

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -13,10 +13,10 @@ const Filters = ({filter, setFilter}: {filter:string, setFilter: any}) => {
       exit={{ opacity: 0, x: 200 }}
       className={`w-full py-10 flex items-center justify-start lg:justify-center  h-auto gap-4 md:gap-8  px-2  overflow-x-scroll scrollbar-hidden  scroll-smooth`}
     >
-      <Button category={{id: 666, name: "Menu", urlParam: "all", icon: <BiRestaurant />}} filter = {filter} setFilter = {setFilter} />
+      <Button category={{_id: 666, name: "Menu", urlParam: "all", icon: <BiRestaurant />}} filter = {filter} setFilter = {setFilter} />
         {
             Categories.map((category: FoodCategory) =>{
-                return <Button key = {category.id} category = {category} filter = {filter} setFilter = {setFilter} />
+                return <Button key = {category._id} category = {category} filter = {filter} setFilter = {setFilter} />
             })
         }
 
